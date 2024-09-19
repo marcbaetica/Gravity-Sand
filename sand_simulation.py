6637eb67090ddbedbfe6187c8c185434dc5702ce
marcbaetica <marc.baetica@gmail.com>
2024-09-19 00:44:05 -0400

    Made graph work.

--- sand_simulation.py
+++ sand_simulation.py
@@ -2,31 +2,35 @@ from random import randint
 
 
 class DisplayMatrix:
-    pixels = None
 
     def __init__(self):
-        self.randomify_pixels()
+        self.pixels = self.randomify_pixels()
 
     def get_pixels_ds(self):
         assert self.pixels is not None, "Expected self.pixels to not be none!"
         return self.pixels
 
-    def randomify_pixels(self):
-        self.pixels = [[0 for _ in range(20)] for _ in range(20)]
-        self.pixels[randint(0, len(self.pixels))][randint(0, len(self.pixels[0]))] = 1
+    @staticmethod
+    def randomify_pixels():
+        pixels_matrix = [[0 for _ in range(20)] for _ in range(20)]
+        pixels_matrix[randint(0, len(pixels_matrix)-1)][randint(0, len(pixels_matrix[0])-1)] = 1
+        return pixels_matrix
 
     def move_down(self):
         self.pixels.insert(0, self.pixels.pop())
 
     def act_gravity_step(self):
         # find one
+        r, c = self.get_coordinates_of_one()
+        if r == len(self.pixels)-1:  # bottom
+            pass
+        else:
+            self.pixels[r][c] = 0
+            self.pixels[r+1][c] = 1
+
+    def get_coordinates_of_one(self):
         for r in range(len(self.pixels)):
             for c in range(len(self.pixels[r])):
                 if self.pixels[r][c] == 1:
-                    coordinates = (r, c)
-                    print(f'One at {coordinates}. len(self.pixels)={len(self.pixels)}.')
-        if coordinates[0] == len(self.pixels)-1:  # bottom
-            pass
-        else:
-            self.pixels[coordinates[0]][coordinates[1]] = 0
-            self.pixels[coordinates[0]+1][coordinates[1]] = 1
+                    print(f'One at ({r}, {c}). len(self.pixels)={len(self.pixels)}.')
+                    return r, c
--- simulator_runner.py
+++ simulator_runner.py
@@ -3,21 +3,59 @@ from pprintpp import pprint
 from sand_simulation import DisplayMatrix
 
 
+# game = DisplayMatrix()
+#
+# for _ in range(20):
+#     pprint(game.get_pixels_ds())
+#     game.act_gravity_step()
+#     sleep(0.25)
+
+
+
+
+# later: https://docs.bokeh.org/en/latest/docs/user_guide/server/library.html#ug-server-library
+
+
+from random import random
+
+from bokeh.layouts import column
+from bokeh.models import Button
+from bokeh.plotting import figure, curdoc
+
+from time import sleep
+
+
 game = DisplayMatrix()
-
-for _ in range(20):
-    pprint(game.get_pixels_ds())
-    game.act_gravity_step()
-    sleep(0.25)
-
-
-from bokeh.plotting import figure, show
-from bokeh.models import glyph
+pixels_matrix = game.get_pixels_ds()
+pprint(pixels_matrix)
+one_r, one_c = game.get_coordinates_of_one()
+print(one_r, one_c)
 
 
+
+# create a plot and style its properties
 plt = figure(width=2550, height=1250, x_range=(-0.5, 20), y_range=(-0.5, 20), x_axis_label='x')
-plt.scatter([0, 1, 2, 3], [1, 6, 8, 9])
+render_obj = plt.scatter(x=[one_c], y=[20-one_r])  # 20-one_r to inverse it as per the print.
 
-# TODO: Extract "1"'s coordinates and punch in graph server.
+plot_data_struct = render_obj.data_source
 
-show(plt)
+# create a callback that adds a number in a random location
+def callback():
+    game.act_gravity_step()
+
+    # BEST PRACTICE --- update .data in one step with a new dict
+    new_data = dict()
+    new_data['x'] = plot_data_struct.data['x']
+    r, _ = game.get_coordinates_of_one()
+    new_data['y'] = [20-r]
+    plot_data_struct.data = new_data
+
+
+# add a button widget and configure with the call back
+button = Button(label="Press Me")
+button.on_event('button_click', callback)
+
+# put the button and plot in a layout and add to the document
+curdoc().add_root(column(button, plt))
+
+# bokeh serve --show simulator_runner.py
