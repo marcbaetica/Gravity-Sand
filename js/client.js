b8f219ac8800424e28fe4a0b5ca0b61d09d2851b
Marc <marc.baetica@gmail.com>
2024-11-16 13:25:24 -0500

    Central grain location.

--- js/client.js
+++ js/client.js
@@ -1,17 +1,11 @@
-let d  = new Date();
+// let d  = new Date();
 // alert("Today's date is " + d);
-console.log(d);
-document.body.innerHTML = "<h1> Today's date is " + d + "</h1>";
+// console.log(d);
+// document.body.innerHTML = "<h1> Today's date is " + d + "</h1>";
 
-// const socket = io('ws://localhost:9999');
-const socket = io('http://localhost:9999');
 
-socket.emit('Hello', 'server!');
-
-socket.on('connection', (socket) => {
-  console.log('socket is connected!');
-  socket.on('disconnect', () => {
-    console.log('socket is disconnected!')
-  })
-})
+const canvas = document.getElementById('section_map');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(395, 395, 10, 10);
 
