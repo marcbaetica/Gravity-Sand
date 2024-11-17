3808c5b2d40e8537c3d76970fb2670765365b98c
Marc <marc.baetica@gmail.com>
2024-11-17 08:12:12 -0500

    Browser to node srv data send.

--- js/client.js
+++ js/client.js
@@ -9,3 +9,29 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
 ctx.fillRect(395, 395, 10, 10);
 
+window.addEventListener('keydown', this.check, false);
+
+function send_keystroke_code(data) {
+  // console.log(data)
+  // console.log(`${data}`)
+  const http = new  XMLHttpRequest()
+  const url = 'http://localhost:8000/keystroke'
+  http.open('POST', url)
+  console.log(`sending data ${data}`)
+  http.send(data)
+  // renew later
+  // http.onreadtstatechange=(e) => {
+    // if (http.status === 200) {
+      // console.log(http.responseText)
+      // return true
+    // }
+  // }
+}
+
+function check(e) {
+  // alert(e.keyCode)
+  // 'w' = e.code:87 and e.code:KeyW
+  console.log(e.keyCode)
+  send_keystroke_code(e.code)
+}
+
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // npm start
 
 
+// required to actually receive data.
+// See https://expressjs.com/en/resources/middleware/body-parser.html
+const bodyParser = require('body-parser')
 const express = require('express')
 const path = require('path')
 // const http = require('http')
@@ -17,6 +20,13 @@ app.get('/', function (req, res) {
   // console.log(__dirname)
 })
 
+app.post('/keystroke', bodyParser.text(), function (req, res) {
+  const key_code = req.body
+  console.log(`keystroke code received is ${key_code}`)
+  // without this, the client (web page) sends payloads,
+  // but server stops receiving any more after 7-ish payloads.
+  res.status(200).send(`keystorke code was ${key_code}`)
+})
 
 // Below is an alternative to leveraging var server = http.createServer(app)
 app.listen(app.get('port'))
