7718d53e6bda44020c5a0b50acc12e1d96cf1ada
Marc <marc.baetica@gmail.com>
2024-11-18 18:27:22 -0500

    Cleanup and working version.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+from pprintpp import pprint
 from websockets.sync.server import serve
 
 
 def echo(websocket):
     for message in websocket:
-        websocket.send(message)
+        pprint(message)
+        websocket.send('Hello Client!')
 
 with serve(echo, 'localhost', 8500) as s_soc:
     s_soc.serve_forever()
