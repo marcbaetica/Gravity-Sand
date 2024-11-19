878198e6a856939c6ecc0a42d078d50ca6cb839c
Marc <marc.baetica@gmail.com>
2024-11-19 08:42:55 -0500

    Added key computation handling.

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 console.log('connecting to the socket')
 const c_soc = new WebSocket('ws://localhost:8500')
-console.log(c_soc)
+console.log(c_soc)  // TODO: A property checks if connection was establshed.
 
 console.log('Sending hello server!')
 c_soc.addEventListener('open', function (event) {
@@ -11,7 +11,24 @@ c_soc.addEventListener('message', function (event) {
   console.log(c_soc)
   console.log(`Message from server:\n${event.data}`)
   console.log(c_soc)
+  var grain_position = eval(event.data)
+  ctx.fillRect(grain_position[0], grain_position[1], 10, 10)
 })
 
 // socket.on('')
+
+
+const canvas = document.getElementById('section_map');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(395, 395, 10, 10);
+
+window.addEventListener('keydown', check, false);
+
+
+function check(e) {
+  // 'w' = e.code:87, key:w and e.code:KeyW
+  console.log(`Pressed key ${e.code}. Sending to server...`)
+  c_soc.send(e.code)
+}
 
--- server.py
+++ server.py
@@ -2,10 +2,31 @@ from pprintpp import pprint
 from websockets.sync.server import serve
 
 
+grain_pos = [400, 400]
+
+def calculate_new_position(key):
+    global GRAIN_POSITION
+    acceptable_keys = ['KeyW', 'KeyA', 'KeyS', 'KeyD']
+    if key not in acceptable_keys:
+        print(f'Key {key} was not in one of the accepted {acceptable_keys}.')
+    # TODO: Account for edges.
+    elif key == acceptable_keys[0]:
+        grain_pos[1] = grain_pos[1] - 10
+    elif key == acceptable_keys[1]:
+        grain_pos[0] = grain_pos[0] - 10
+    elif key == acceptable_keys[2]:
+        grain_pos[1] = grain_pos[1] + 10
+    elif key == acceptable_keys[3]:
+        grain_pos[0] = grain_pos[0] + 10
+    return grain_pos
+
+
 def echo(websocket):
-    for message in websocket:
-        pprint(message)
-        websocket.send('Hello Client!')
+    for key in websocket:
+        print(f'Key pressed was: {key}')
+        print(f'Sending {grain_pos}.')
+        websocket.send(str(calculate_new_position(key)))
+
 
 with serve(echo, 'localhost', 8500) as s_soc:
     s_soc.serve_forever()
