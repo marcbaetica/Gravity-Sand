d97ae8a9a661232c0fd7e0feb214168a7dbce1d5
Marc <marc.baetica@gmail.com>
2024-11-19 15:38:14 -0500

    Raw map render.

--- canvas_computation.py
+++ canvas_computation.py
@@ -1,12 +1,35 @@
 import numpy as np
 from random import randint
+from websockets.sync.server import serve
 
 
-a = np.zeros((10, 10))
+def generate_map():
+    a = np.zeros((80, 80))
+    for _ in range(100):
+        a[randint(0, 79)][randint(0, 79)] = 2
+    a[5][5] = 1
+    print(a)
+    print(str(a))
+    return a
 
-for _ in range(20):
-    a[randint(0, 9)][randint(0, 9)] = 2
-a[5][5] = 1
 
-print(a)
+# def echo(websocket):
+    # for key in websocket:
+        # websocket.send(str(generate_map()))
+
+import json
+from time import sleep
+
+
+def echo(websocket):
+    for _ in range(1):
+        sleep(1)
+        map = generate_map()
+        print(map)
+        map_str = json.dumps(map.tolist())
+        print(map_str)
+        websocket.send(map_str)
+
+with serve(echo, 'localhost', 8500) as s_soc:
+    s_soc.serve_forever()
 
--- websocket_base/js/client.js
+++ websocket_base/js/client.js
@@ -3,25 +3,16 @@ const c_soc = new WebSocket('ws://localhost:8500')
 console.log(c_soc)  // TODO: A property checks if connection was establshed.
 
 console.log('Sending hello server!')
-// c_soc.addEventListener('open', function (event) {
-//   c_soc.send('Hello Server!')
-// })
+c_soc.addEventListener('open', function (event) {
+  c_soc.send('Hello Server!')
+})
 
 c_soc.addEventListener('message', function (event) {
-  // console.log(c_soc)
+  console.log(c_soc)
   console.log(`Message from server:\n${event.data}`)
-  // console.log(c_soc)
-  console.log(typeof event.data)
-  var data = event.data
-  var map = JSON.parse(data)
-  for (var y=0; y<80; y++) {
-    for (var x=0; x<80; x++) {
-      if (map[y][x] == 2) {
-        console.log('found 2!')
-        ctx.fillRect(y*10, x*10, 10, 10)
-      }
-    }
-  }
+  console.log(c_soc)
+  var grain_position = eval(event.data)
+  ctx.fillRect(grain_position[0], grain_position[1], 10, 10)
 })
 
 // socket.on('')
